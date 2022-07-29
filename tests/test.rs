471794cd7e68e1e69b7d34b55d21351c86380b5f
Noel Kwan <noelkwan1998@gmail.com>
2022-07-29 12:31:23 +0800

    libtest mimic with nextest compat

---
+++ tests/test.rs
@@ -0,0 +1,28 @@
+extern crate libtest_mimic;
+
+use libtest_mimic::{Arguments, Test, Outcome, run_tests};
+
+
+// Parse command line arguments
+
+// Run all tests and exit the application appropriatly (in this case, the
+// test runner is a dummy runner which does nothing and says that all s
+// passed).
+fn main() {
+    let args = Arguments::from_args();
+
+    // Create a list of tests (in this case: three dummy tests)
+    let tests = vec![
+        Test::test("toph"),
+        Test::test("sokka"),
+        Test {
+            name: "long_computation".into(),
+            kind: "".into(),
+            is_ignored: true,
+            is_bench: false,
+            data: (),
+        },
+    ];
+
+    run_tests(&args, tests, |test| Outcome::Passed).exit();
+}
